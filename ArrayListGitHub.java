09ae68998b5498ea50e98a949343d28908df12bc
Nisarga Shashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2018-11-27 10:31:57 -0500

    Combining Lists Method Created
    This creates a new method that takes two ArrayLists and combines them into one new list, which gets returned to the main method.

--- ArrayListGitHub.java
+++ ArrayListGitHub.java
@@ -5,4 +5,15 @@ public class ArrayListGitHub{
 		ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 		ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(10,12,13,14,15,16,17,18,19,20));
 	}
+
+	public static ArrayList<Integer> combine(ArrayList<Integer> listOne, ArrayList<Integer> listTwo){
+		ArrayList<Integer> combinedList = new ArrayList<Integer>();
+		for(int x = 0; x < listOne.size(); x++){
+			combinedList.add(listOne.get(x));
+		}
+		for(int x = 0; x < listTwo.size(); x++){
+			combinedList.add(listTwo.get(x));
+		}
+		return combinedList;
+	}
 }
