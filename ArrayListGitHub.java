dd76a7d7faf5f40b705f42a69dd801cc5410fbcb
Nisarga Shashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2018-11-27 10:37:27 -0500

    Test Combine Method
    Thsi commit tests the code written so far by creating a new variable in the main method (called combined) to hold the returned ArrayList from the combine method, and then printing out the contents of each variable on a new line. It also changes the contents of list2.

--- ArrayListGitHub.java
+++ ArrayListGitHub.java
@@ -3,7 +3,13 @@ import java.util.*;
 public class ArrayListGitHub{
 	public static void main(String[]args){
 		ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
-		ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(10,12,13,14,15,16,17,18,19,20));
+		ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(10,11,12,13,14,15,16,17,18,19));
+
+		System.out.println(list1);
+		System.out.println(list2);
+
+		ArrayList<Integer> combined = combine(list1, list2);
+		System.out.println(combined);
 	}
 
 	public static ArrayList<Integer> combine(ArrayList<Integer> listOne, ArrayList<Integer> listTwo){
@@ -16,4 +22,6 @@ public class ArrayListGitHub{
 		}
 		return combinedList;
 	}
+
+
 }
